ca8a28a70bb6db97ecfb1789017b0a41e655d0e1
Shaunzy <68426146+lightstrike93@users.noreply.github.com>
2024-01-26 17:31:58 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://excelCarpet.github.io',
-})
+  integrations: [tailwind(), compress(),]
+});
